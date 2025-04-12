3972d7334ae77024a994d17057f75d49af0f4028
Real_Marshal <remarshal@protonmail.com>
2025-04-12 19:39:39 +0300

    fix build

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import { Canvas } from '@/components/canvas/canvas'
+import { Suspense } from 'react'
 
 export default function Home() {
   return (
     <main className='flex flex-col gap-10 items-center justify-center'>
       <h1>ffraid plan</h1>
-      <Canvas />
+      <Suspense>
+        <Canvas />
+      </Suspense>
     </main>
   )
 }
